46ee411dc1691349b95d6b5ea12c9817150dd54d
Aria Groult <aria.groult@outlook.fr>
2018-11-20 19:21:29 +0100

    added marvel comics related reducers

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,10 +1,11 @@
 import authentication from "./auth.reducer"
-import marvel from "./marvelApi.reducer"
+import marvelApiReducers from "./marvelApi.reducer"
 //import theme from './theme.reducer'
 import { combineReducers } from "redux"
 
 
 export const rootReducer = combineReducers({
 	authentication,
-	marvel
+	 marvelAuth: marvelApiReducers.marvelAuthentication,
+	 marvelComics: marvelApiReducers.marvelComics
 })
--- src/Redux/reducers/marvelApi.reducer.js
+++ src/Redux/reducers/marvelApi.reducer.js
@@ -3,51 +3,78 @@ import {marvelAPIActions} from "../actions/marvelApi.actions"
 
 //
 const initialState = {
-	baseUrl: "https://gateway.marvel.com//v1/public/",
-	apiKeys : {
-		private : "",
-		public : ""
-	},
-	comics: {
-		array: [],// Fiil with ids
-		status: "",
-		offset: 0,
-		// Adds up comics object by ids
-	},
-	characters: {
-		array: [],// Fiil with ids
-		status: "",
-		offset: 0,
-		// Adds up characters object by ids
-	},
-	events: {
-		array: [],// Fiil with ids
-		status: "",
-		offset: 0,
-		// Adds up events object by ids
-	},
-	series: {
-		array: [],// Fiil with ids
-		status: "",
-		offset: 0,
-		// Adds up series object by ids
-	}
+	 baseUrl: "https://gateway.marvel.com//v1/public/",
+	 apiKeys: {
+			private: "",
+			public: ""
+	 },
+	 comics: {
+			array: [],// Fiil with ids
+			isLoading: false,
+			offset: 0,
+			// Adds up comics object by ids
+	 },
+	 characters: {
+			array: [],// Fiil with ids
+			isLoading: false,
+			offset: 0,
+			// Adds up characters object by ids
+	 },
+	 events: {
+			array: [],// Fiil with ids
+			isLoading: false,
+			offset: 0,
+			// Adds up events object by ids
+	 },
+	 series: {
+			array: [],// Fiil with ids
+			isLoading: false,
+			offset: 0,
+			// Adds up series object by ids
+	 }
 }
 
-
-export default (state=initialState, action) => {
-	switch (action.type) {
-	case marvelAPIActions.CONNECT_API_DONE :
-		return {
-			...state,
-			apiKeys: action.payload
-		}
-	case marvelAPIActions.DISCONNECT_API_DONE :
-		return {
-			...state,
-			apiKeys: action.payload
-		}
-	default :
-		return state
-	}
+const marvelApiReducers = {
+	 marvelAuthentication: (state = initialState, action) => {
+			switch (action.type) {
+				 case marvelAPIActions.CONNECT_API_DONE :
+						return {
+							 ...state,
+							 apiKeys: action.payload
+						}
+				 case marvelAPIActions.DISCONNECT_API_DONE :
+						return {
+							 ...state,
+							 apiKeys: action.payload
+						}
+				 default:
+						return state
+			}
+	 },
+	 
+	 marvelComics: (state = initialState.comics, action) => {
+			switch (action.type) {
+				 case marvelAPIActions.FETCH_COMICS:
+						return {
+							 ...state,
+							 loading: true
+						}
+				 case marvelAPIActions.FETCH_COMICS_SUCCESS:
+						return {
+							 ...state,
+							 loading: false,
+							 array: [...action.payload]
+						}
+				 case marvelAPIActions.FETCH_COMICS_FAILURE:
+						return {
+							 ...state,
+							 loading: false,
+							 array: []
+						}
+				 default:
+						return state
+			}
+	 }
 }
+
+export default marvelApiReducers
