cd0a40c6fe24f6dadf0e0d0d35c2f80c1caeb381
Aria Groult <aria.groult@outlook.fr>
2018-11-23 12:44:58 +0100

    added view to fix IOS problem of app beeing overlayed with statusbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@
  */
 
 import React, {Component} from "react"
+import {Platform, StyleSheet, View} from "react-native"
 import RootStack from "./Routes/Routes"
 import {Provider} from "react-redux"
 import {configureStore} from "~/Redux/store"
@@ -15,13 +16,22 @@ const reduxStore = configureStore({})
 
 
 export default class App extends Component {
-		
-		
 		render() {
 				return (
 					<Provider store={reduxStore}>
-							<RootStack/>
+							<View style={{height: "100%"}}>
+									<View style={styles.statusBarBackground}>
+									</View>
+									<RootStack/>
+							</View>
 					</Provider>
-				)
+		)
 		}
-}
+		}
+		
+		const styles = StyleSheet.create({
+				statusBarBackground: {
+						backgroundColor: "#800000",
+						height: (Platform.OS === "ios") ? 20 : 0, //this is just to test if the platform is iOS to give it a height of 18, else, no height (Android apps have their own status bar)
+		}
+		})
